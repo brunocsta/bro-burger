181d4644632394cf8164e00e80e0dee4a05ecd5f
Bruno Costa <b.costamartins@gmail.com>
2024-10-24 21:20:53 -0300

    Adds functionality to add and update cart, also preemptively handles duplicate products in cart. Fixes cart structure for functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@ const cartCounter = document.querySelector("#cart-count");
 const addressInput = document.querySelector("#address");
 const addressWarn = document.querySelector("#address-warn");
 
+let cart = [];
+
 cartBtn.addEventListener("click", function () {
   cartModal.classList.add("flex");
   cartModal.classList.remove("hidden");
@@ -25,3 +27,56 @@ closeModalBtn.addEventListener("click", function () {
   cartModal.classList.add("hidden");
   cartModal.classList.remove("flex");
 });
+
+menu.addEventListener("click", function (e) {
+  let parentButton = e.target.closest(".add-to-cart-btn");
+
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price")).toFixed(
+      2
+    );
+
+    addToCart(name, price);
+  }
+});
+
+function addToCart(name, price) {
+  const existingItem = cart.find((item) => item.name === name);
+  if (existingItem) {
+    existingItem.quantity += 1;
+    return;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
+  updateCartModal();
+}
+
+function updateCartModal() {
+  let total = 0;
+
+  cart.forEach((item) => {
+    const cartItemElement = document.createElement("div");
+    cartItemElement.innerHTML = `
+      <div>
+        <div>
+          <p>${item.name}</p>
+          <p>${item.quantity}</p>
+          <p>${item.price}</p>
+        </div>
+  
+        <div>
+          <button>
+            Remover
+          </button>
+        </div>
+      </div>
+    `;
+
+    cartItemsContainer.appendChild(cartItemElement);
+  });
+}
